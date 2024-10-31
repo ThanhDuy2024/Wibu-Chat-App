63195a5917c40e90f70b56cd45a1903ae2c1f792
Lam Thanh Duy <thanhduythptthanhbinh@gmail.com>
2024-10-31 21:40:52 +0700

    Chat Messages Design

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -26,7 +26,35 @@ const Chat = () => {
                     <img src="./info.png" alt="" />
                 </div>
             </div>
-            <div className="center"></div>
+            <div className="center">
+                <div className='message'>
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <p>Lorem ipsum dolor.</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className='message own'>
+                    <div className="texts">
+                        <img src="" alt="" />
+                        <p>Lorem ipsum dolor.</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className='message'>
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <p>Lorem ipsum dolor.</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className='message own'>
+                    <div className="texts">
+                        <p>Lorem ipsum dolor.</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+            </div>
             <div className="bottom">
                 <div className="icons">
                     <img src="./img.png" alt="" />
